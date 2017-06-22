0411acd13370312f49d07792a1c87dd315078c8a
piercegwang <piercegwang@users.noreply.github.com>
2017-06-21 21:11:01 -0700

    added comments, fixed black jack

--- blackjack/black_jack.rb
+++ blackjack/black_jack.rb
@@ -1,27 +1,24 @@
 #!/usr/bin/ruby
+=begin
+This program takes a command line input of a string that represents a blackjack hand, and then returns the value of the hand
+=end
 
 def black_jack_hand(hand)
 	sum = 0
-	rem_a = []
-	as = []
-	values = { "2" => 2, "3" => 3, "4" => 4, "5" => 5, "6" => 6, "7" => 7, "8" => 8, "9" => 9, "T" => 10, "J" => 10, "D" => 10, "K" => 10}
-	splithand = hand.split('')
-	splithand.each do |x|
-		if x != "A"
-			rem_a.push(x)
-		else
-			as.push(x)
-		end
+	a = false
+	values = { "2" => 2, "3" => 3, "4" => 4, "5" => 5, "6" => 6, "7" => 7, "8" => 8, "9" => 9, "T" => 10, "J" => 10, "D" => 10, "K" => 10, "A" => 1} 
+	#set default values of each card
+	splithand = hand.split('') #split the hand into a workable array
+	splithand.each do |x| 
+		sum += values[x]
+		if x == "A" then a = true end
 	end
-	rem_a.each {|y| sum += values[y] }
-	as.each do |z|
-		if sum > 21
-			sum += 11
-		else
-			sum += 1
-		end
+	#add cards all together with all aces counting as one first
+	#now if I can add the rest of one ace, I will. Otherwise, return the sum.
+	if sum <= 11 and a == true
+		sum += 10
 	end
-	puts sum
+	return sum.to_s + "\n"
 end
 
-black_jack_hand("AAT")
+print black_jack_hand(ARGV[0])
--- day03/ex04/persons_of_interest.rb
+++ day03/ex04/persons_of_interest.rb
@@ -1,11 +1,13 @@
 #!/usr/bin/ruby
+=begin
+This program takes an input of the hash women_in_science and prints their names along with the date of their births. It does this by putting the values of the first hash in their own array then sorting them by their year of birth. Finally, it accesses them using the index and prints out the values along with the preset text
+=end
 
 def great_births(x)
 	names = []
-	x.each {|k,v| names.push(v)}
-	names.sort_by! { |hash| hash[:year_of_birth] }
-	#puts names
-	names.each do |p|
+	x.each {|k,v| names.push(v)} #put values in seperate array, x
+	names.sort_by! { |hash| hash[:year_of_birth] } #sort values of array, x, by the symbol :year_of_birth
+	names.each do |p| #print each name and birth year with preset text
 		print "#{p[:name].to_s} is a great person born in #{p[:year_of_birth].to_s}.\n"
 	end
 end
