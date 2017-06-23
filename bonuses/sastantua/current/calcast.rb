693dada6168511298526e5225b702f66313b76bb
piercegwang <piercegwang@users.noreply.github.com>
2017-06-23 16:39:48 -0700

    Finished for day04
    In the middle of working on the tictactoe program. Need to add win check method and break loop.

---
+++ bonuses/sastantua/current/calcast.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/ruby
+
+input = ARGV[0].to_i #Input format level
+def calcLines(y) #Find total lines
+	@x = y
+	@totalLines = 0
+	@tierNumLines = 3
+	while @x > 0
+		@totalLines += @tierNumLines
+		@tierNumLines += 1
+		@x -= 1
+	end
+	return @totalLines
+end
+
+def numberAstr(line)
+	@regAstr = 2*(line-1)+1
+	@x = line
+	@totalLines = 0
+	@tierNumLines = 3
+	while @x > 0
+		@x -= @tierNumLines
+		@tierNumLines += 1
+	end
+	@x += @tierNumLines-1
+	#@x now == the remaining number of lines in the pyramid
+	#now to calc extra astrs
+	
+		#use counter for finding the extra astr value for each line
+		#have total astr variable to keep track of all the total astr needed
+end
+
+#Main function
+def pyramid(x)
+	puts numberAstr(calcLines(x))
+end
+pyramid(input)
